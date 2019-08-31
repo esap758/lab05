a0e01b2e0737188ee0f25d1d510c28eb50905001
Ella Sapiatzer <esap758@aucklanduni.ac.nz>
2019-08-31 16:34:19 +1200

    "fixed factorial"

--- esap758_280_Lab2_in_Lab5/test_factorial.py
+++ esap758_280_Lab2_in_Lab5/test_factorial.py
@@ -4,9 +4,9 @@ import unittest
 class FactorialTest(unittest.TestCase):
     def test_factorial(self):
         num = 10
-        factorial = factorial(Num)
+        factor = maths.factorial(num)
         
-        self.assertEqual(factorial, 3628800)
+        self.assertEqual(factor, 3628800)
         
         
 if __name__ == '__main__':
